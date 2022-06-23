0ca72994d53a7ea95f86ecab848793e1deeb0c99
Sania <saniazkhaja@gmail.com>
2022-06-22 22:12:52 -0500

    emotionagifs addition

--- emotionalgifs/index.js
+++ emotionalgifs/index.js
@@ -1,4 +1,5 @@
 const multipart = require('parse-multipart');
+const fetch = require('node-fetch');
 
 
 module.exports = async function (context, req) {
@@ -14,10 +15,51 @@ const body = req.body;
 // parse the body
 const parts = multipart.Parse(body, boundary);
 
-let convertedResult = Buffer.from(parts[0].data).toString('base64');
+// let convertedResult = Buffer.from(parts[0].data).toString('base64');
 
+    // context.res = {
+    //     // status: 200, /* Defaults to 200 */
+    //     body: convertedResult
+    // };
+
+    //module.exports function
+//analyze the image
+    const result = await analyzeImage(parts[0].data);
     context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: convertedResult
+        body: {
+            result
+        }
     };
+    console.log(result)
+    context.done(); 
 }
+
+async function analyzeImage(img){
+    // const subscriptionKey = process.env.SUBSCRIPTIONKEY;
+    // const uriBase = process.env.ENDPOINT + '/face/v1.0/detect';
+
+    const subscriptionKey = '50cd864af0074326a37150ed20a9e431';
+    const uriBase = 'https://saniasfaceapi.cognitiveservices.azure.com' + '/face/v1.0/detect';
+
+    let params = new URLSearchParams({
+        'returnFaceId': 'true',
+        'returnFaceAttributes': 'blur'     //FILL IN THIS LINE
+    })
+
+    let resp = await fetch(uriBase + '?' + params.toString(), {
+        method: 'POST',  //WHAT TYPE OF REQUEST?
+        body: img,  //WHAT ARE WE SENDING TO THE API?
+        headers: {
+            'Content-Type': 'application/octet-stream',
+            "Ocp-Apim-Subscription-Key": subscriptionKey
+        }
+    })
+    let data = await resp.json();
+    
+    return data; 
+}
+
+// 50cd864af0074326a37150ed20a9e431
+// https://saniasfaceapi.cognitiveservices.azure.com/
+
+// process.env.varName
