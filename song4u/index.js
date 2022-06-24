c938a857230cdb76575513c8de8155345a22badf
Sania <saniazkhaja@gmail.com>
2022-06-23 22:36:34 -0500

    added age to pic

--- song4u/index.js
+++ song4u/index.js
@@ -1,13 +1,67 @@
 const querystring = require('qs');
+const fetch = require('node-fetch');
 
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     const queryObject = querystring.parse(req.body);
-
     const url = queryObject.MediaUrl0;
-    context.log(url);
+
+    const resp = fetch(url, {
+        method: "GET",
+    })
+
+    const data = await resp.arrayBuffer();
+
+    const result = await analyzeImage(data, context);
+
+    const age = result[0].faceAttributes.age;
+
+    let id = "";
+
+    if (age > 5 && age < 25) {
+        id = "GenZ";
+    } else if (age > 24 && age < 41) {
+        id = "GenY";
+    } else if (age > 40 && age < 57) {
+        id = "GenX";
+    } else if (age > 56 && age < 76) {
+        id = "BabyBoomers";
+    } else {
+        id = "Unknown";
+    }
+
+    context.log(id);
+    
     context.res = {
-        body: url
+        body: id,
     };
+}
+
+async function analyzeImage(img, context){
+    const KEY = process.env["FACE_API_KEY"];
+    const URI_BASE = process.env["FACE_API_URI"] + "/face/v1.0/detect";
+	// env variables (similar to .gitignore/.env file) to not expose personal info
+
+    const params = new URLSearchParams({
+        returnFaceId: "true",
+        returnFaceAttributes: "age",
+    })
+
+    // making the post request
+    const resp = await fetch(URI_BASE + '?' + params.toString(),{
+        method: 'POST',
+        body: img,
+        // img is the parameter inputted
+        headers: {
+            'Content-Type' : 'application/octet-stream',
+            // HOW DO YOU AUTHENTICATE?
+            "Ocp-Apim-Subscription-Key" : KEY,
+        }
+    })
+
+    // receive the response
+    const data = await resp.json();
+
+    return data;
 }
