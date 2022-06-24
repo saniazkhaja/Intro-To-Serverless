b3f6873c0ade64c29b6d90196837cf0132d8ca3e
Sania <saniazkhaja@gmail.com>
2022-06-23 20:12:35 -0500

    song4u twilio

--- song4u/index.js
+++ song4u/index.js
@@ -5,9 +5,9 @@ module.exports = async function (context, req) {
 
     const queryObject = querystring.parse(req.body);
 
-    const url =  queryObject.MediaUrl0;
+    const url = queryObject.MediaUrl0;
     context.log(url);
     context.res = {
-        body: url,
+        body: url
     };
 }
