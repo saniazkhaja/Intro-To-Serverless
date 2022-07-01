bec1d6fde52a66978c1d65742a4ec46b13527952
Sania <saniazkhaja@gmail.com>
2022-07-01 13:56:58 -0500

    adding file with error checking

--- bunnimage/index.js
+++ bunnimage/index.js
@@ -3,6 +3,9 @@ const bunnForm = document.getElementById('bunnForm');
 bunnForm.addEventListener('submit', function (event) {
    event.preventDefault()
    const username = document.getElementById("username").value
+   if (username == "") {
+        alert("No name error.")
+   }
    const output = document.getElementById("output")
    output.textContent = username + "❤"
 });
