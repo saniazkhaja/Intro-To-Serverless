003f40059323f9dc4af712f9fcd497ba1b7a2fcb
Sania <saniazkhaja@gmail.com>
2022-07-01 12:51:08 -0500

    sharing secrets

--- deepsecrets/index.js
+++ deepsecrets/index.js
@@ -1,12 +1,79 @@
+const { ItemResponse } = require('@azure/cosmos');
 const qs = require('qs');
+const CosmosClient = require("@azure/cosmos").CosmosClient;
+
+const config = {
+    endpoint: process.env.COSMOS_ENDPOINT,
+    key: process.env.COSMOS_KEY,
+    databaseId: "SecretStorer",
+    containerId: "secrets",
+    partitionKey: {kind: "Hash", paths: ["/secrets"]}
+};
+
+  async function create(client, databaseId, containerId) {
+    const partitionKey = config.partitionKey;
+  
+    /**
+     * Create the database if it does not exist
+     */
+    const { database } = await client.databases.createIfNotExists({
+      id: databaseId
+    });
+    console.log(`Created database:\n${database.id}\n`);
+  
+    /**
+     * Create the container if it does not exist
+     */
+    const { container } = await client
+      .database(databaseId)
+      .containers.createIfNotExists(
+        { id: containerId, partitionKey },
+        { offerThroughput: 400 }
+      );
+  
+    console.log(`Created container:\n${container.id}\n`);
+  }
+
+  async function createDocument(newItem) {
+    const { endpoint, key, databaseId, containerId } = config;
+
+    const client = new CosmosClient({ endpoint, key });
+
+    const database = client.database(databaseId);
+    const container = database.container(containerId);
+
+    // Make sure Tasks database is already setup. If not, create it.
+    await create(client, databaseId, containerId);
+    
+    console.log(`Querying container: Items`);
+
+    // query to return all items
+    const querySpec = {
+    query: "SELECT top 1 * FROM c order by c._ts desc"
+    };
+
+    // read all items in the Items container
+    const { resources: items } = await container.items
+    .query(querySpec)
+    .fetchAll();
+
+    const { resource: createdItem } = await container.items.create(newItem);
+
+    return items;
+  }
+  
 
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     const queryObject = qs.parse(req.body);
     context.log(queryObject);
+    let message = queryObject.Body; // this is the user's input
+    let document = {"message": message} // create an object with the string `"message"` as the key, and the variable `message` as its value
+    let items = await createDocument(document)// call the createDocument function with the document we just made
 
+    const responseMessage = `Thanks 😊! Stored your secret "${message}". 😯 Someone confessed that: ${JSON.stringify(items[0].message)}`
     context.res = {
-        body: queryObject.Body
+        body: responseMessage
      };
 }
