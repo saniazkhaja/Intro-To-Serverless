07944baf10bf623af1c17cb3101c581fb5cf3c49
Sania <saniazkhaja@gmail.com>
2022-07-15 21:35:57 -0500

    changes

--- final-project/index.js
+++ final-project/index.js
@@ -13,16 +13,19 @@ button.addEventListener("click", async function() {
 })
 
 
-module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+module.exports = async function (context, req, secrets) {
+    // context.log('JavaScript HTTP trigger function processed a request.');
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+    // const name = (req.query.name || (req.body && req.body.name));
+    // const responseMessage = name
+    //     ? "Hello, " + name + ". This HTTP triggered function executed successfully."
+    //     : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: responseMessage
+        body: secrets
     };
+
+
+
 }
